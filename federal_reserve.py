c2ebf7b4f93aff0c62f2c0de7ebdb94b35c27ba3
P Guardiario <pguardiario@gmail.com>
2016-12-21 11:05:03 +0800

    pep8

--- federal_reserve.py
+++ federal_reserve.py
@@ -1,5 +1,6 @@
 import scrapy
 
+
 class FederalReserveSpider(scrapy.Spider):
     name = 'federal-reserve-spider'
     start_urls = ['https://www.federalreserve.gov/releases/h10/hist/']
@@ -7,11 +8,11 @@ class FederalReserveSpider(scrapy.Spider):
     def parse(self, response):
         for tr in response.css('.statistics tr'):
             href = tr.css('a[href]::attr(href)').extract_first()
-            request = scrapy.Request(response.urljoin(href), callback=self.parse_link)
+            request = scrapy.Request(response.urljoin(href),
+                                     callback=self.parse_link)
             request.meta['country'] = tr.css('a[href]::text').extract_first()
             request.meta['currency'] = tr.css('td::text').extract_first()
             yield request
-
 
     def parse_link(self, response):
         for tr in response.css('.statistics tr'):
